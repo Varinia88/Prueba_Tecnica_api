f5e912771a13dd11cd1299d81ebee6125bb5a2d9
Varinia Polar Zevallos <varinia0088@gmail.com>
2023-10-27 09:18:50 +0200

    Se agrega el proyecto

--- cypress/e2e/Contar_pets.cy.js
+++ cypress/e2e/Contar_pets.cy.js
@@ -1,8 +1,7 @@
 describe('Contar mascotas que se llamen igual',()=>{
 
     const filePath = 'cypress/support/datos.js'
-    const pet = []
-    const pets = []
+  
 
     it('Llamamos al json',()=>{
         cy.readFile(filePath)
